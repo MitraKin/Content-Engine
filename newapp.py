ee445ae2a42214b1c5d82afbe13b9b2303197f4f
Kinnaur Mitra <kinnaurm249@protonmail.com>
2024-11-22 02:00:46 +0530

    first commit

---
+++ newapp.py
@@ -0,0 +1,183 @@
+import streamlit as st
+import logging
+import os
+import ollama
+
+
+from langchain_community.document_loaders import UnstructuredPDFLoader
+from langchain_ollama import OllamaEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_ollama.chat_models import ChatOllama
+from langchain_core.runnables import RunnablePassthrough
+from langchain.retrievers.multi_query import MultiQueryRetriever
+from typing import List, Tuple, Dict, Any, Optional
+
+# Set protobuf environment variable to avoid error messages
+
+os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
+
+
+
+st.set_page_config(
+    page_title="Ollama PDF RAG Streamlit UI",
+    page_icon="🎈",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+logger = logging.getLogger(__name__)
+
+@st.cache_resource(show_spinner=True)
+def extract_model_names(models_info: Dict[str, List[Dict[str, Any]]]) -> Tuple[str, ...]:
+    logger.info("Extracting model names from models_info")
+    model_names = tuple(model["name"] for model in models_info["models"])
+    logger.info(f"Extracted model names: {model_names}")
+    return model_names
+
+def load_pdf_files_from_directory(directory_path: str) -> Chroma:
+    logger.info(f"Loading all PDF files from directory: {directory_path}")
+    pdf_files = [os.path.join(directory_path, f) for f in os.listdir(directory_path) if f.endswith('.pdf')]
+
+    if not pdf_files:
+        st.error("No PDF files found in the specified directory.")
+        logger.error("Directory does not contain any PDF files.")
+        return None
+
+    all_chunks = []
+    for pdf_file in pdf_files:
+        logger.info(f"Processing file: {pdf_file}")
+        loader = UnstructuredPDFLoader(file_path=pdf_file)
+        data = loader.load()
+
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=7500, chunk_overlap=100)
+        chunks = text_splitter.split_documents(data)
+        all_chunks.extend(chunks)
+
+    embeddings = OllamaEmbeddings(model="nomic-embed-text")
+    vector_db = Chroma.from_documents(
+        documents=all_chunks,
+        embedding=embeddings,
+        collection_name="myRAG"
+    )
+    logger.info("Vector DB created from directory")
+    return vector_db
+
+
+
+def process_question(question: str, vector_db: Chroma, selected_model: str) -> str:
+    logger.info(f"Processing question: {question} using model: {selected_model}")
+    
+    llm = ChatOllama(model=selected_model)
+    
+    QUERY_PROMPT = PromptTemplate(
+        input_variables=["question"],
+        template="""You are an AI language model assistant. Your task is to generate 2
+        different versions of the given user question to retrieve relevant documents from
+        a vector database. By generating multiple perspectives on the user question, your
+        goal is to help the user overcome some of the limitations of the distance-based
+        similarity search. Provide these alternative questions separated by newlines.
+        Original question: {question}""",
+    )
+
+    retriever = MultiQueryRetriever.from_llm(
+        vector_db.as_retriever(), 
+        llm,
+        prompt=QUERY_PROMPT
+    )
+
+    template = """Answer the question based ONLY on the following context:
+    {context}
+    Question: {question}
+    """
+
+    prompt = ChatPromptTemplate.from_template(template)
+
+    chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    response = chain.invoke(question)
+    logger.info("Question processed and response generated")
+    return response
+
+
+def main() -> None:
+    st.subheader("🤖AI PDF READER : Read and Analyze Documents OFFLINE ", divider="gray", anchor=False)
+
+    models_info = ollama.list()
+    available_models = extract_model_names(models_info)
+
+    col1, col2 = st.columns([1.5, 2])
+
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = []
+    if "vector_db" not in st.session_state:
+        st.session_state["vector_db"] = None
+    if "use_sample" not in st.session_state:
+        st.session_state["use_sample"] = False
+
+    if available_models:
+        selected_model = col2.selectbox(
+            "Pick a model available locally on your system ↓", 
+            available_models,
+            key="model_select"
+        )
+
+    file_upload = r"E:\Content-Engine\Documents"
+    if st.session_state["vector_db"] is None:
+        with st.spinner("Processing PDFs from the directory..."):
+            st.session_state["vector_db"] = load_pdf_files_from_directory(file_upload)
+
+    if st.session_state["vector_db"] is not None:
+        st.success("PDF files processed successfully. You can now ask questions!")
+
+    with col1:
+        message_container = st.container(height=500, border=True)
+
+        for i, message in enumerate(st.session_state["messages"]):
+            avatar = "🤖" if message["role"] == "assistant" else "😎"
+            with message_container.chat_message(message["role"], avatar=avatar):
+                st.markdown(message["content"])
+
+        if prompt := st.chat_input("Enter a prompt here...", key="chat_input"):
+            try:
+                st.session_state["messages"].append({"role": "user", "content": prompt})
+                with message_container.chat_message("user", avatar="😎"):
+                    st.markdown(prompt)
+
+                with message_container.chat_message("assistant", avatar="🤖"):
+                    with st.spinner(":green[processing...]"):
+                        if st.session_state["vector_db"] is not None:
+                            response = process_question(
+                                prompt, st.session_state["vector_db"], selected_model
+                            )
+                            st.markdown(response)
+                        else:
+                            st.warning("Please upload a PDF file first.")
+
+                if st.session_state["vector_db"] is not None:
+                    st.session_state["messages"].append(
+                        {"role": "assistant", "content": response}
+                    )
+
+            except Exception as e:
+                st.error(e, icon="⛔️")
+                logger.error(f"Error processing prompt: {e}")
+        else:
+            if st.session_state["vector_db"] is None:
+                st.warning("Upload a PDF file or use the sample PDF to begin chat...")
+
+if __name__ == "__main__":
+    main()
